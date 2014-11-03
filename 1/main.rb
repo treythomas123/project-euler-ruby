9651ee01e768c009e5c79c177c5241ad997b50fe
treythomas123 <treythomas123@gmail.com>
2014-11-03 11:17:49 -0700

    Wrap lines to 80 columns max

--- 1/main.rb
+++ 1/main.rb
@@ -3,13 +3,14 @@
 # Solution to https://projecteuler.net/problem=1
 # Trey Thomas
 #  
-# | If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
-# | The sum of these multiples is 23
+# | If we list all the natural numbers below 10 that are multiples of 3 or 5, we
+# | get 3, 5, 6 and 9. The sum of these multiples is 23
 # |
 # | Find the sum of all the multiples of 3 or 5 below 1000
 #
-# This solution works through the problem in the most obvious way -- go through all the numbers less than 1000 and 
-# add them to a running total if they're divisible by 3 or 5.
+# This solution works through the problem in the most obvious way -- go through 
+# all the numbers less than 1000, and add them to a running total if they're 
+# divisible by 3 or 5.
 
 
 def multiple_of_3_or_5(num)
