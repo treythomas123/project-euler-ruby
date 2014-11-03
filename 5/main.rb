8d6dae87ab7f642700900ad081b51b2f576e9faf
treythomas123 <treythomas123@gmail.com>
2014-11-03 14:28:20 -0700

    Add problem 5

---
+++ 5/main.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/ruby
+
+# Solution to https://projecteuler.net/problem=5
+# Trey Thomas
+#  
+# | 2520 is the smallest number that can be divided by each of the numbers from 
+# | 1 to 10 without any remainder.
+# | 
+# | What is the smallest positive number that is evenly divisible by all of the 
+# | numbers from 1 to 20?
+# 
+# Tried a little code golf for this problem... I had this, but susepcted  it 
+# could be made shorter somehow:
+# l=1;(1..20).each{|i|l=l.lcm(i)};p l
+#
+# Googling led me to the reduce function, which works perfectly here and pretty
+# much does the exact same thing. It's also much more readable.
+
+p (1..20).reduce(:lcm)
+
