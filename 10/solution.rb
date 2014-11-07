d4326ad17894279e711d9cd4b5c585a51bc5ff95
treythomas123 <treythomas123@gmail.com>
2014-11-06 20:42:45 -0700

    Add solution to problem 10

--- 10/solution.rb
+++ 10/solution.rb
@@ -7,6 +7,15 @@
 # |
 # | Find the sum of all the primes below two million.
 #
-#
+# Simple solution using standard library prime generator
 
+require 'Prime'
 
+gen = Prime::instance.lazy
+sum = 0
+
+while (prime = gen.next) < 2_000_000
+  sum += prime
+end
+
+puts sum
